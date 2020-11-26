f91415f429553b8a28c69fc3c234dc8b2e25ff12
SINANE Mohamad <m7sinane@enib.fr>
2020-11-26 17:36:56 +0100

    Add Top Floor

--- js/musee.js
+++ js/musee.js
@@ -38,48 +38,67 @@ function peuplerScene(){
 	var sol = creerSol("sol",{},scene) ; 
 
 	createStrucureForMuseum();
-	
-	var tableau = creerPoster("tableau1",{tableau:"assets/tableaux/Berthe.jpg"},scene) ;
-	tableau.position.z = -0.1  ;  
-	tableau.position.y = 1.7 ; 
+
 }
 
 
 function createStrucureForMuseum()
 {
-	var side_1 = createWall(30,6,2,4);
-	var side_2 = createWall(30,6,0,0);
-	var side_3 = createWall(30,6,0,0);
-	var side_4 = createWall(30,6,0,0);
+	var sideBottomFloor_1 = createWall(30,6,2,4);
+	var sideBottomFloor_2 = createWall(30,6,0,0);
+	var sideBottomFloor_3 = createWall(30,6,0,0);
+	var sideBottomFloor_4 = createWall(30,6,0,0);
 	
-	side_1.rotation = new BABYLON.Vector3(-1.57,0,0)
-	side_2.rotation = new BABYLON.Vector3(-1.57,0,0)
-	side_3.rotation = new BABYLON.Vector3(-1.57,1.57,0)
-	side_4.rotation = new BABYLON.Vector3(-1.57,1.57,0)
+	sideBottomFloor_1.rotation = new BABYLON.Vector3(-1.57,0,0);
+	sideBottomFloor_2.rotation = new BABYLON.Vector3(-1.57,0,0);
+	sideBottomFloor_3.rotation = new BABYLON.Vector3(-1.57,1.57,0);
+	sideBottomFloor_4.rotation = new BABYLON.Vector3(-1.57,1.57,0);
 	
-	side_2.position = new BABYLON.Vector3(0,0,-30)
-	side_4.position = new BABYLON.Vector3(30,0,0)
+	sideBottomFloor_2.position = new BABYLON.Vector3(0,0,-30);
+	sideBottomFloor_4.position = new BABYLON.Vector3(30,0,0);
 
 	var midle_1 = createWall(10,6, 2,4);
 	var midle_2 = createWall(10,6, 2,4);
 	var midle_3 = createWall(10,6, 2,4);
 	
-	midle_1.rotation = new BABYLON.Vector3(-1.57,0,0)
-	midle_2.rotation = new BABYLON.Vector3(-1.57,0,0)
-	midle_3.rotation = new BABYLON.Vector3(-1.57,0,0)
+	midle_1.rotation = new BABYLON.Vector3(-1.57,0,0);
+	midle_2.rotation = new BABYLON.Vector3(-1.57,0,0);
+	midle_3.rotation = new BABYLON.Vector3(-1.57,0,0);
 
-	midle_1.position = new BABYLON.Vector3(0, -0.1,-15)
-	midle_2.position = new BABYLON.Vector3(10,-0.1,-15)
-	midle_3.position = new BABYLON.Vector3(20,-0.1,-15)
+	midle_1.position = new BABYLON.Vector3(0, -0.1,-15);
+	midle_2.position = new BABYLON.Vector3(10,-0.1,-15);
+	midle_3.position = new BABYLON.Vector3(20,-0.1,-15);
 	
 	var midle_1_2 = createWall(15,6,0,0);
 	var midle_2_3 = createWall(15,6,0,0);
 
-	midle_1_2.rotation = new BABYLON.Vector3(-1.57,1.57,0)
-	midle_2_3.rotation = new BABYLON.Vector3(-1.57,1.57,0)
+	midle_1_2.rotation = new BABYLON.Vector3(-1.57,1.57,0);
+	midle_2_3.rotation = new BABYLON.Vector3(-1.57,1.57,0);
 
-	midle_1_2.position = new BABYLON.Vector3(20, 0,-15)
-	midle_2_3.position = new BABYLON.Vector3(10,0,-15)
+	midle_1_2.position = new BABYLON.Vector3(20, 0,-15);
+	midle_2_3.position = new BABYLON.Vector3(10,0,-15);
+
+	var roofBottomFloor = createWall(30,30,0,0);
+	roofBottomFloor.position = new BABYLON.Vector3(0, 6,-30);
+
+	var sideTopFloor_1 = createWall(30,6,0,0);
+	var sideTopFloor_2 = createWall(30,6,0,0);
+	var sideTopFloor_3 = createWall(30,6,0,0);
+	var sideTopFloor_4 = createWall(30,6,0,0);
+	
+	
+	sideTopFloor_1.rotation = new BABYLON.Vector3(-1.57,0,0);
+	sideTopFloor_2.rotation = new BABYLON.Vector3(-1.57,0,0);
+	sideTopFloor_3.rotation = new BABYLON.Vector3(-1.57,1.57,0);
+	sideTopFloor_4.rotation = new BABYLON.Vector3(-1.57,1.57,0);
+	 
+	sideTopFloor_1.position = new BABYLON.Vector3(0,6,0);
+	sideTopFloor_3.position = new BABYLON.Vector3(0,6,0);
+	sideTopFloor_2.position = new BABYLON.Vector3(0,6,-30);
+	sideTopFloor_4.position = new BABYLON.Vector3(30,6,0);
+
+	var roofTopFloor = createWall(30,30,0,0);
+	roofTopFloor.position = new BABYLON.Vector3(0, 12,-30);
 
 }
 
